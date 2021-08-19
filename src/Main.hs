61860b803e057b60c606517f5822e6b5a01006ae
Stephen Ahmad <privat.ahmad@gmail.com>
2021-08-19 15:33:39 +0000

    Changes.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,16 @@
 module Main where
+import System.IO
 import MyModule(someFunc)
+
+-- factorial:: (Num a, Eq a) => a -> a
+factorial 1 = 1
+factorial k = k * factorial (k-1)
 
 main :: IO ()
 main = do
     someFunc
+    let r = factorial 200000
+    let t = show r
+    print t
+    -- writeFile "number.txt" t
     putStrLn "Hello, Haskell!"
